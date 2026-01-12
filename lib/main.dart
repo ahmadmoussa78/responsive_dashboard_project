2db2c1200e26dffedee0997dc1384b789406d652
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-12 17:17:04 +0200

    Creating the Project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveDashborad extends StatelessWidget {
+  const ResponsiveDashborad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+  
