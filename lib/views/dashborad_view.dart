70b58c6ce5cee544f04d58f6a9f8eb66d1d1e0be
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-13 08:59:04 +0200

    Added Desktop Dashboard Layout

--- lib/views/dashborad_view.dart
+++ lib/views/dashborad_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/dashborad_desktop_layout.dart';
 
 class DashboradView extends StatelessWidget {
   const DashboradView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletlayout: (context) => SizedBox(),
+        desktopLayout: (context) => DashboradDesktopLayout(),
+      ),
+    );
   }
 }
---
+++ lib/widgets/dashborad_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboradDesktopLayout extends StatelessWidget {
+  const DashboradDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
