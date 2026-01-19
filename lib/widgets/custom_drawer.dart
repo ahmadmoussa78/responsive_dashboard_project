d4047795f93b4e071c0a8107d4927638c2c0179f
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-19 12:57:02 +0200

    Added settings&logout in draweritems

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/active_and_inactive_items.dart';
 import 'package:responsive_dashboard/widgets/drawer_item_list_view.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
@@ -19,6 +21,20 @@ class CustomDrawer extends StatelessWidget {
           ),
           SizedBox(height: 8),
           DrawerItemListView(),
+          Expanded(child: SizedBox()),
+          InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+              title: "Setting system",
+              image: Assets.imagesSettings,
+            ),
+          ),
+          InActiveDrawerItem(
+            drawerItemModel: DrawerItemModel(
+              title: "Logout account",
+              image: Assets.imagesLogout,
+            ),
+          ),
+          SizedBox(height: 48),
         ],
       ),
     );
