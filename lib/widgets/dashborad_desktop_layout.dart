4e139bdccb921fd7f353321316390a94102428e4
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-22 08:13:29 +0200

    Added Quick invoice widget

--- lib/widgets/dashborad_desktop_layout.dart
+++ lib/widgets/dashborad_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expenses.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/widgets/quick_invoice.dart';
 
 class DashboradDesktopLayout extends StatelessWidget {
   const DashboradDesktopLayout({super.key});
@@ -13,7 +14,12 @@ class DashboradDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child: Column(children: [Expanded(child: AllExpenses())]),
+          child: Column(
+            children: [
+              AllExpenses(),
+              QuickInvoice(),
+            ],
+          ),
         ),
       ],
     );
