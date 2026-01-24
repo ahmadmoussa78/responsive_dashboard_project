8ac4e57f4846a9e3dd159da57a87d9d1338f4b42
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-24 13:32:05 +0200

    Added ListTile to MyCard

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -17,6 +19,26 @@ class MyCard extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadiusGeometry.circular(12),
           ),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 31,
+                top: 16,
+                right: 42,
+              ),
+              title: Text(
+                "Name",
+                style: AppStyles.styleRegular16.copyWith(color: Colors.white),
+              ),
+              subtitle: const Text(
+                "Syah Bandi",
+                style: AppStyles.styleMedium20,
+              ),
+              trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+          ],
         ),
       ),
     );
