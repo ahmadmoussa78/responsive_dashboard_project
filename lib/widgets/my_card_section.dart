5737b760b967231643bb68f6de2a20f1a85bc1df
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-25 10:35:11 +0200

    Added Page controller to handle dots indicator

--- lib/widgets/my_card_section.dart
+++ lib/widgets/my_card_section.dart
@@ -3,8 +3,26 @@ import 'package:responsive_dashboard/utils/app_styles.dart';
 import 'package:responsive_dashboard/widgets/dots_indicator.dart';
 import 'package:responsive_dashboard/widgets/my_card_page_view.dart';
 
-class MyCardSection extends StatelessWidget {
+class MyCardSection extends StatefulWidget {
   const MyCardSection({super.key});
+
+  @override
+  State<MyCardSection> createState() => _MyCardSectionState();
+}
+
+class _MyCardSectionState extends State<MyCardSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
