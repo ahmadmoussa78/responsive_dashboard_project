dcd7a311767e0b23b4bc97d027a09d0ee1a8c626
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-29 10:41:01 +0200

    Add DetailedIncomeChart widget and integrate it into IncomeSectionBody

---
+++ lib/widgets/detailed_income_chart.dart
@@ -0,0 +1,72 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
+}
+
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
+  int activeIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1, child: PieChart(getChartData()));
+  }
+
+  PieChartData getChartData() {
+    return PieChartData(
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (p0, pieTouchResponse) {
+          activeIndex =
+              pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
+      sectionsSpace: 0,
+      sections: [
+        PieChartSectionData(
+          titlePositionPercentageOffset: activeIndex == 0 ? 1.5 : null,
+          title: activeIndex == 0 ? 'Design service' : '40%',
+          titleStyle: AppStyles.styleMedium16.copyWith(color: Colors.black),
+          showTitle: true,
+          radius: activeIndex == 0 ? 60 : 50,
+          color: const Color(0XFF208CC8),
+          value: 40,
+        ),
+        PieChartSectionData(
+          radius: activeIndex == 1 ? 60 : 50,
+
+          color: const Color(0XFF4EB7F2),
+          value: 25,
+          titlePositionPercentageOffset: activeIndex == 1 ? 2.1 : null,
+          title: activeIndex == 1 ? 'Design product' : '25%',
+          titleStyle: AppStyles.styleMedium16.copyWith(color: Colors.black),
+          showTitle: true,
+        ),
+        PieChartSectionData(
+          titlePositionPercentageOffset: activeIndex == 2 ? 1.4 : null,
+          title: activeIndex == 2 ? 'Product royalti' : '22%',
+          titleStyle: AppStyles.styleMedium16.copyWith(color: Colors.black),
+          showTitle: true,
+          radius: activeIndex == 2 ? 60 : 50,
+
+          color: const Color(0Xff064061),
+          value: 20,
+        ),
+        PieChartSectionData(
+          titlePositionPercentageOffset: activeIndex == 3 ? 1.6 : null,
+          title: activeIndex == 3 ? 'Other' : '40%',
+          titleStyle: AppStyles.styleMedium16.copyWith(color: Colors.black),
+          showTitle: true,
+          radius: activeIndex == 3 ? 60 : 50,
+          color: const Color(0XFFE2DECD),
+          value: 22,
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/income_section_body.dart
+++ lib/widgets/income_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/utils/size_config.dart';
+import 'package:responsive_dashboard/widgets/detailed_income_chart.dart';
 import 'package:responsive_dashboard/widgets/income_chart.dart';
 import 'package:responsive_dashboard/widgets/income_details.dart';
 
@@ -11,7 +12,10 @@ class IncomeSectionBody extends StatelessWidget {
     double width = MediaQuery.sizeOf(context).width;
 
     return width >= SizeConfig.desktop && width <= 1750
-        ? const SizedBox()
+        ? const Padding(
+            padding: EdgeInsets.all(16),
+            child: DetailedIncomeChart(),
+          )
         : const SizedBox(
             height: 200,
             child: Row(
